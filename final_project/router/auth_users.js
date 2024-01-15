8c6b9644ad59b0c46c0f644377398d9b08d290aa
bettygitbie <107159318+bettygitbie@users.noreply.github.com>
2024-01-15 05:52:56 -0500

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,22 +7,73 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username,password)=>{
+    //write code to check if username and password match the one we have in records.
+    let validusers = users.filter((user)=>{
+      return (user.username === username && user.password === password)
+    });
+    if(validusers.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
 
+ 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!username || !password) {
+    return res.status(404).json({message: "Error logging in"});
+  }
+  if (authenticatedUser(username,password)) {
+  let accessToken = jwt.sign({
+    data: password
+  }, 'access', { expiresIn: 60 * 60 });
+  req.session.authorization = {
+    accessToken,username
+}
+return res.status(200).send("User successfully logged in");
+} else {
+  return res.status(208).json({message: "Invalid Login. Check username and password"});
+}
+ // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let review = req.body.review;
+  let isbnReceived = req.params.isbn;
+  let reviewer = req.session.authorization['username'];
+  let selectedBooks = books[isbn];
+  if(!isbn) {
+    return res.status(403).send("Invalid isbn entered")
+  }
+  if(selectedBooks) {
+    if(review) {
+         selectedBooks['review'][reviewer] = review;
+    books[isbn] = selectedBooks;
+    }
+   else {
+    res.send("Review is empty!")
+   }
+   res.send(`The review for book with isbn ${isbnReceived} is added/changed.`)
+  }
+  res.send("isbn is not defined for book")
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.authenticated = regd_users;
